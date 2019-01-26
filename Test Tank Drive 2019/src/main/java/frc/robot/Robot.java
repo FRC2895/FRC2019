fd43ef1e6367a7b8b5f4295a7953d060a6192963
Joe Pokorny <team2895@outlook.com>
2019-01-26 13:04:21 -0500

    Initial commit. Able to drive robot.

---
+++ Test Tank Drive 2019/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  private DifferentialDrive m_myRobot;
+  private Joystick m_stick;
+
+  @Override
+  public void robotInit() {
+    m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
+    m_stick = new Joystick(0);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    m_myRobot.arcadeDrive(-m_stick.getY(),m_stick.getX());
+  }
+}
